55b20d4b2878e8d835496fd0549dbb576e5d1b4e
Panagiotis Kalfopoulos <giotiskalfo@gmail.com>
2018-11-19 21:37:56 +0200

    Initial commit

---
+++ DatabaseHandler/DatabaseHelper.cs
@@ -0,0 +1,80 @@
+﻿namespace OroCampo.DatabaseHandler
+{
+    using OroCampo.Models.Database;
+    using System;
+    using System.Data.SqlClient;
+    using System.Threading.Tasks;
+    using Dapper;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class DatabaseHelper
+    {
+        public async Task<Guid> SaveProduct(Product product, string connectionString)
+        {
+            // We create an sql connection 
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                // Open the connection async
+                await sqlConnection.OpenAsync();
+
+                var query =
+                    "INSERT INTO [dbo].[Product] ([DateTime], [Name], [Description], [Photo], [CategoryId]) OUTPUT INSERTED.Id ";
+                query += "VALUES (GETDATE(), @name, @description, @photo, @categoryId)";
+
+                var insertedId = await sqlConnection.QuerySingleAsync<Guid>(
+                                     query,
+                                     new
+                                     {
+                                         name =product.Name,
+                                         description = product.Description,
+                                         photo = product.Photo,
+                                         categoryId = product.CategoryId
+                                     });
+
+                // Close the connection with database
+                sqlConnection.Close();
+
+                // Return id
+                return insertedId;
+            }
+        }
+
+        public async Task<Product> GetProduct(Guid productId, string connectionString)
+        {
+            // We create an sql connection 
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                // Open the connection async
+                await sqlConnection.OpenAsync();
+
+                var query = "SELECT * FROM [dbo].[Product] (NOLOCK) WHERE [ProductId] = @productId ";
+
+                var product = await sqlConnection.QueryFirstOrDefaultAsync<Product>(query, new { productId });
+
+                sqlConnection.Close();
+
+                return product;
+            }
+        }
+
+        public async Task<List<Product>> GetRoutePoints(string connectionString)
+        {
+            // We create an sql connection 
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                // Open the connection async
+                await sqlConnection.OpenAsync();
+
+                var query = "SELECT * FROM [dbo].[Product] (NOLOCK)";
+
+                var products = await sqlConnection.QueryAsync<Product>(query);
+
+                sqlConnection.Close();
+
+                return products.ToList();
+            }
+        }
+
+    }
+}
---
+++ OroCampo.Models/Database/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace OroCampo.Models.Database
+{
+    using System;
+
+    public class Product
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Photo { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public Guid CategoryId { get; set; }
+    }
+}
