6368755099983316fcd511d40bd6ad2c139e79fb
Dimitrios Kaltzidis <kaltzidis.dimitrios@gmail.com>
2018-12-27 21:20:45 +0200

    Adds custom theme to site and new controllers

---
+++ OroCampo.WebSite/Controllers/BlogPostsController.cs
@@ -0,0 +1,13 @@
+﻿namespace OroCampo.WebSite.Controllers
+{
+    using System.Web.Mvc;
+
+    public class BlogPostsController : Controller
+    {
+        // GET: BlogPosts
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
